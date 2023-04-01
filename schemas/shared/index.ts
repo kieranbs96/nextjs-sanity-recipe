38b168dc83a5c91c06127061a2614b175b0ac905
Kieran.Smith <Kieran.Smith@global.com>
2023-04-01 13:01:27 +0100

    fix studio

--- schemas/shared/index.ts
+++ schemas/shared/index.ts
@@ -1,9 +1,8 @@
 import { SchemaTypeDefinition } from 'sanity';
 import author from './author';
-import products from './products';
 import status from './status';
 import blockContent from './blockContent';
 
-const sharedSchema: SchemaTypeDefinition[] = [author, status, products, blockContent];
+const sharedSchema: SchemaTypeDefinition[] = [author, status, blockContent];
 
 export default sharedSchema;
